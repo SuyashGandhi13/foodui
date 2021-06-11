4339245d9ad79777f50bda92b73a242a3d76797d
Snooozeeee <suyash758@gmail.com>
2021-06-11 18:23:53 +0530

    improvement to looks

---
+++ lib/FoodCards.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class FoodCard extends StatelessWidget {
+  final String source;
+  final String text;
+  final int price;
+  final Color cardColor;
+  final Color textColor;
+
+  FoodCard({this.source,this.text,this.price,this.cardColor,this.textColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 10.0),
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: cardColor,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 80.0,
+            width: 80.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              image: DecorationImage(
+                  image: AssetImage(source),
+                  fit: BoxFit.contain),
+            ),
+          ),
+          SizedBox(height: 30),
+          Text(
+            text,
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              fontSize: 21,
+              fontWeight: FontWeight.w700,
+              color: textColor,
+            ),
+          ),
+          Text(
+            '\$ $price',
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              fontSize: 21,
+              fontWeight: FontWeight.w700,
+              color: textColor),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foodui/FoodCards.dart';
 
 class HomeScreen extends StatelessWidget {
+  final String s = "\$ 21";
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
@@ -51,7 +53,7 @@ class HomeScreen extends StatelessWidget {
                   borderSide: BorderSide.none,
                 ),
                 filled: true,
-                fillColor: Color(0xFFCDC9C9),
+                fillColor: Color(0xFFEDEDED),
                 hintText: 'Search',
                 hintStyle: TextStyle(color: Colors.grey),
                 prefixIcon: Icon(
@@ -67,7 +69,37 @@ class HomeScreen extends StatelessWidget {
                 fontFamily: 'Source Sans Pro',
                 fontSize: 24.0,
                 letterSpacing: 0.5,
-                fontWeight: FontWeight.w300,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Container(
+              height: 250,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  FoodCard(
+                    source: 'images/burger.png',
+                    text: 'Hamburg',
+                    price: 21,
+                    cardColor: Color(0xFFFFEAC5),
+                    textColor: Color(0xFF786138),
+                  ),
+                  FoodCard(
+                    source: 'images/french fry.png',
+                    text: 'Fries',
+                    price: 15,
+                    cardColor: Color(0xFFC3E3FF),
+                    textColor: Color(0xFF375A78),
+                  ),
+                  FoodCard(
+                    source: 'images/donut.png',
+                    text: 'Donut',
+                    price: 7,
+                    cardColor: Color(0xFFD7FBD9),
+                    textColor: Color(0xFF437846),
+                  ),
+                ],
               ),
             )
           ],
